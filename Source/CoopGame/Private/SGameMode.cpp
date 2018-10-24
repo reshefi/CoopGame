7aee81d11adf89ead819cb000098d14f488026ca
Ido <reshef_i@hotmail.com>
2018-10-24 05:45:59 +0300

    Initial Wave of spawns
    Initial Wave of spawns

--- Source/CoopGame/Private/SGameMode.cpp
+++ Source/CoopGame/Private/SGameMode.cpp
@@ -4,4 +4,50 @@
 
 
 
+ASGameMode::ASGameMode()
+{
+	TimeBetweenWaves = 2.0;
+}
 
+
+void ASGameMode::StartWave()
+{
+	WaveCount++;
+
+	NrOfBotsToSpawn = 2 * WaveCount;
+
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGameMode::SpawnBotTimeElapsed, 1.0f, true, 0.0f);
+}
+
+void ASGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+	PrepareForNextWave();
+}
+
+
+void ASGameMode::PrepareForNextWave()
+{
+	FTimerHandle TimerHandle_NextWaveStart;
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+}
+
+void ASGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	PrepareForNextWave();
+}
+
+void ASGameMode::SpawnBotTimeElapsed()
+{
+	SpawnNewBot();
+
+	NrOfBotsToSpawn--;
+
+	if (NrOfBotsToSpawn <= 0)
+	{
+		EndWave();
+	}
+}
